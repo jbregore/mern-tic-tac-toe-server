8c2d46aca8bc6eab3179452cee023e96590c8e81
jb <jbbbregore099@gmail.com>
2024-02-16 13:18:27 +0800

    setup conenction

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
-import { PORT } from "./src/config.js";
+import { PORT, mongoDbUrl } from "./src/config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+mongoose
+  .connect(mongoDbUrl)
+  .then(() => {
+    console.log("App connected to the database");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log("error : ", err);
+  });
--- src/config.js
+++ src/config.js
@@ -3,3 +3,4 @@ import dotenv from "dotenv";
 dotenv.config({ path: "../.env" });
 
 export const PORT = process.env.PORT;
+export const mongoDbUrl = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONBODB_PASSWORD}@${process.env.MONGODB_DATABASE}/?retryWrites=true&w=majority`;
