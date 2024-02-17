aef3fb8e1d70ee68b0293663eca96ce67b21858d
jb <jbbbregore099@gmail.com>
2024-02-17 14:43:10 +0800

    invitations

--- src/sockethandler/socketHandler.js
+++ src/sockethandler/socketHandler.js
@@ -8,6 +8,7 @@ export const socketHandler = (io) => {
       if (!onlineUsers.some((user) => user.user.uuid === newUser.uuid)) {
         onlineUsers.push({ user: newUser, socketId: socket.id });
       }
+      // console.log("socketId ", socket.id);
       io.emit("get-users", onlineUsers);
     });
 
@@ -19,6 +20,28 @@ export const socketHandler = (io) => {
     socket.on("offline", () => {
       onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
       io.emit("get-users", onlineUsers);
+    });
+
+    //game invitation
+    socket.on("invite:user", (inviter, invitedUserId) => {
+      const invitedSocket = onlineUsers.find(
+        (user) => user.user.uuid === invitedUserId
+      );
+
+      if (invitedSocket) {
+        io.to(invitedSocket?.socketId).emit("game:invitation", inviter);
+      }
+    });
+
+    // game decline
+    socket.on("invite:decline", (decliner, inviterUser) => {
+      const inviterSocket = onlineUsers.find(
+        (user) => user.user.uuid === inviterUser
+      );
+
+      if (inviterSocket) {
+        io.to(inviterSocket?.socketId).emit("game:decline", decliner);
+      }
     });
   });
 };
