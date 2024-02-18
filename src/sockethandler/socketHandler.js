b08e431ace11729820368f77b08209a7142c2b31
jb <jbbbregore099@gmail.com>
2024-02-18 16:39:31 +0800

    handling rematch

--- src/sockethandler/socketHandler.js
+++ src/sockethandler/socketHandler.js
@@ -2,11 +2,17 @@ import { User } from "../models/User.js";
 
 export const socketHandler = (io) => {
   let onlineUsers = [];
+  let game = {
+    players: [],
+    agreedPlayers: [],
+    agreedPlayersCount: 0,
+  };
 
   io.on("connection", (socket) => {
     socket.on("set-user", (newUser) => {
       if (!onlineUsers.some((user) => user.user.uuid === newUser.uuid)) {
         onlineUsers.push({ user: newUser, socketId: socket.id });
+        game.players.push(newUser.uuid);
       }
       // console.log("socketId ", socket.id);
       io.emit("get-users", onlineUsers);
@@ -33,6 +39,17 @@ export const socketHandler = (io) => {
       }
     });
 
+    // game invite cancelled
+    socket.on("invite:cancel", (invitedUser) => {
+      const inviterSocket = onlineUsers.find(
+        (user) => user.user.uuid === invitedUser.uuid
+      );
+
+      if (inviterSocket) {
+        io.to(inviterSocket?.socketId).emit("game:close");
+      }
+    });
+
     // game decline
     socket.on("invite:decline", (decliner, inviterUser) => {
       const inviterSocket = onlineUsers.find(
@@ -54,5 +71,120 @@ export const socketHandler = (io) => {
         io.to(inviterSocket?.socketId).emit("game:start", invitedUser);
       }
     });
+
+    //gameplay
+    socket.on("gameplay", (turn, me, opponent, boardData) => {
+      // console.log("turn ", turn);
+      // console.log("me ", me);
+      // console.log("opponent ", opponent);
+      // console.log("boardData ", boardData);
+      const opponentSocket = onlineUsers.find(
+        (user) => user.user.uuid === opponent.uuid
+      );
+
+      if (opponentSocket) {
+        io.to(opponentSocket?.socketId).emit(
+          "gameplay:updated",
+          turn,
+          me,
+          opponent,
+          boardData
+        );
+      }
+    });
+
+    socket.on("gameplay:finished", (hasWinner, winner, loser) => {
+      if (hasWinner) {
+        //insert data to the database
+        const winnerSocket = onlineUsers.find(
+          (user) => user.user.uuid === winner.uuid
+        );
+
+        io.to(winnerSocket?.socketId).emit(
+          "gameplay:done",
+          "Congratulations you won.",
+          true
+        );
+
+        const loserSocket = onlineUsers.find(
+          (user) => user.user.uuid === loser.uuid
+        );
+
+        io.to(loserSocket?.socketId).emit("gameplay:done", "You lose.", false);
+      } else {
+        //insert data to the database
+
+        const winnerSocket = onlineUsers.find(
+          (user) => user.user.uuid === winner.uuid
+        );
+
+        io.to(winnerSocket?.socketId).emit(
+          "gameplay:done",
+          "It's a draw! The game ends in a tie."
+        );
+
+        const loserSocket = onlineUsers.find(
+          (user) => user.user.uuid === loser.uuid
+        );
+
+        io.to(loserSocket?.socketId).emit(
+          "gameplay:done",
+          "It's a draw! The game ends in a tie."
+        );
+      }
+    });
+
+    socket.on("continue:agreed", (user, opponent, isContinue) => {
+      const userSocket = onlineUsers.find((u) => u.user.uuid === user.uuid);
+
+      const opponentSocket = onlineUsers.find(
+        (u) => u.user.uuid === opponent.uuid
+      );
+
+      if (isContinue) {
+        game.agreedPlayers.push(user);
+      }
+
+      game.agreedPlayersCount++;
+
+      const allPlayersInGameAgreed = checkPlayersAgreed(
+        game.players,
+        game.agreedPlayers
+      );
+
+      if (
+        !isContinue &&
+        !allPlayersInGameAgreed &&
+        game.agreedPlayersCount == 2
+      ) {
+        io.to(userSocket?.socketId).emit("cancel:new_match");
+        io.to(opponentSocket?.socketId).emit("cancel:new_match", user);
+        game.agreedPlayers = [];
+        game.agreedPlayersCount = 0;
+      }
+
+      if (allPlayersInGameAgreed) {
+        io.to(userSocket?.socketId).emit("start:new_match");
+        io.to(opponentSocket?.socketId).emit("start:new_match");
+
+        game.agreedPlayers = [];
+        game.agreedPlayersCount = 0;
+      } else if (!allPlayersInGameAgreed && game.agreedPlayersCount == 2) {
+        io.to(userSocket?.socketId).emit("cancel:new_match");
+        io.to(opponentSocket?.socketId).emit("cancel:new_match", user);
+        game.agreedPlayers = [];
+        game.agreedPlayersCount = 0;
+      } else {
+      }
+    });
   });
 };
+
+function checkPlayersAgreed(playersInGameUUIDs, playersWhoAgreed) {
+  const allPlayersInGame =
+    playersWhoAgreed.every((player) =>
+      playersInGameUUIDs.includes(player.uuid)
+    ) && playersWhoAgreed.length === 2;
+
+  return allPlayersInGame;
+}
