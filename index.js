6b5c4d2123a180e830c65a36ef7e00574e0ed0a8
jb <jbbbregore099@gmail.com>
2024-02-19 15:44:32 +0800

    minor

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { PORT, mongoDbUrl } from "./src/utils/config.js";
+import { PORT, clientURL, mongoDbUrl, socketPORT } from "./src/utils/config.js";
 import mongoose from "mongoose";
 import { Server } from "socket.io";
 import { createServer } from "http";
@@ -14,10 +14,10 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: clientURL,
   },
 });
-io.listen(4000);
+io.listen(socketPORT);
 
 app.use(cors());
 app.use(express.json());
--- src/utils/config.js
+++ src/utils/config.js
@@ -5,3 +5,5 @@ dotenv.config({ path: "../.env" });
 export const PORT = process.env.PORT;
 export const mongoDbUrl = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONBODB_PASSWORD}@${process.env.MONGODB_DATABASE}/?retryWrites=true&w=majority`;
 export const jwtSecret = process.env.JWT_SECRET;
+export const clientURL = process.env.CLIENT_URL;
+export const socketPORT = process.env.SOCKET_PORT;
